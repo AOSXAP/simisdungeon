a108a55ef5391557d45b80db1d197578050800d4
AOSXAP <74103250+AOSXAP@users.noreply.github.com>
2020-12-19 21:26:00 +0200

    Update App.js

--- simi_chat/src/App.js
+++ simi_chat/src/App.js
@@ -9,13 +9,7 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyBZpblHaO-aPZQowIpJxMTOO8pvjgah3R8",
-    authDomain: "reactnative-f7bd3.firebaseapp.com",
-    databaseURL: "https://reactnative-f7bd3.firebaseio.com",
-    projectId: "reactnative-f7bd3",
-    storageBucket: "reactnative-f7bd3.appspot.com",
-    messagingSenderId: "720581141542",
-    appId: "1:720581141542:web:23c918b7c08704446a72e7",
+    // your own data here
   });
 }
 
